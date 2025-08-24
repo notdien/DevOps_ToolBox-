5ebe95100187915f0530553c7321c9d15e6476d6
Dien Tran <dien@Diens-MacBook-Pro.local>
2025-08-24 19:00:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Suite for simple DevOps tools
---
+++ compress/compressFile.sh
@@ -0,0 +1,11 @@
+# file name
+echo "Enter compressed file name:"
+read user_file
+echo "User file: $user_file"
+
+# directory source
+echo "Enter where file is:"
+read src_file
+echo "File Source: $src_file"
+
+tar -zcvf "${user_file}-$(date +%d-%m-%Y).tar.gz" "$src_file"
